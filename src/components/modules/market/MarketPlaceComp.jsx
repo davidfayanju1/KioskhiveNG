8ddee18dd0d228b94158893e67f304e6277bc659
davidfayanju1 <davidfayanju01@gmail.com>
2024-12-12 14:58:49 +0100

    All uui changes

--- src/components/modules/market/MarketPlaceComp.jsx
+++ src/components/modules/market/MarketPlaceComp.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { productModel } from "../../../data/ProductData";
+import { Link } from "react-router-dom";
 
 const MarketPlaceComp = () => {
+  const items = [
+    {
+      icon: "/svgs/delivery.svg",
+      name: "FAST DELIVERY",
+      subText: "Delivery in 24hrs",
+    },
+
+    {
+      icon: "/svgs/return.svg",
+      name: "24 HOURS RETURN  ",
+      subText: "100% money-back guarantee",
+    },
+
+    {
+      icon: "/svgs/payment.svg",
+      name: "SECURE PAYMENT",
+      subText: "Your money is safe",
+    },
+
+    {
+      icon: "/svgs/support24.svg",
+      name: "SUPPORT 24/7",
+      subText: "Live contact/message",
+    },
+  ];
+
   return (
-    <div className="h-screen md:px-[4rem] px-[1rem] pt-3">
+    <div className="min-h-screen md:px-[4rem] px-[1rem] pt-3">
       <section className="image_section rounded-xl md:flex-row flex-col min-h-[20rem] w-full flex items-center gap-8 justify-between">
         <img
           src="/images/headHero.png"
@@ -14,6 +42,54 @@ const MarketPlaceComp = () => {
           <img src="/images/header3.png" alt="" className="w-full" />
         </div>
       </section>
+
+      <section className="card_section rounded-[6px] border-solid border-[1px] border-[#E4E7E9] py-[2rem] my-[4rem] flex md:items-center items-start gap-[3rem] justify-between px-[4rem] md:flex-row flex-wrap flex-col">
+        {items.map((item) => (
+          <div
+            className="card flex items-center gap-5 justify-start"
+            key={item.name}
+          >
+            <img src={item.icon} alt="" className="self-start" />
+            <div className="texts">
+              <span className="block font-bold text-black">{item.name}</span>
+              <small className="light text-gray-500">{item.subText}</small>
+            </div>
+          </div>
+        ))}
+      </section>
+
+      <section className="bestdeal">
+        <div className="item_flex flex mb-7 items-center justify-between">
+          <div className="title_container flex items-center">
+            <span className="block font-bold text-[1.5rem]">Best Deals</span>
+          </div>
+          <div className="all-product-link flex items-center gap-1 justify-center">
+            <Link to={"/products/all"} className="block text-[#FC9A30]">
+              Browse all products
+            </Link>
+            <img src="/svgs/right-arrow.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center gap-8 flex-wrap flex-col md:flex-row">
+          {productModel.map((item) => (
+            <div
+              className="card rounded-[8px] md:max-w-[15rem] w-[80%] h-[19rem] border-solid border-[1px]"
+              key={item.name}
+            >
+              <img src={item.img} alt="" className="h-[50%] mx-auto" />
+              <div className="text_container px-2">
+                <h1 className="mb-5 truncate">{item.name}</h1>
+                <small className="block font-bold text-[#FC9A30]">
+                  {item.price}
+                </small>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="product_card"></section>
     </div>
   );
 };
---
+++ src/data/ProductData.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const productModel = [
+  {
+    name: "Dell Optiplex 7000x7480 All-in-One Computer Monitor",
+    img: "/images/products/product1.png",
+    price: "$299.00",
+  },
+  {
+    name: "Sony DSCHX8 High Zoom Point & Shoot Camera",
+    img: "/images/products/product2.png",
+    price: "$1299.00",
+  },
+  {
+    name: "Portable Wshing Machine, 11lbs capacity Model 18NMFIAM",
+    img: "/images/products/product3.png",
+    price: "$70.00",
+    prevPrice: "$866.50",
+  },
+  {
+    name: "2-Barrel Carburetor Carb 2100 Engine Increase Horsepower",
+    img: "/images/products/product4.png",
+    price: "$70.00",
+    prevPrice: "$866.50",
+  },
+  {
+    name: "JBL FLIP 4 - Waterproof Portable Bluetooth Speaker - Black",
+    img: "/images/products/product4.png",
+    price: "$250.00",
+    prevPrice: "$350.50",
+  },
+];
